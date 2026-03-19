70c9587776e6814e6d5587864c68b14fbe08a6e7
oliverfritz-dev <oliver-fritz02@outlook.de>
2026-03-19 15:07:44 +0100

    Initial Commit

---
+++ Pfaukuss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pfaukuss
+//
+//  Created by Privat on 19.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pfaukuss/PfaukussApp.swift
@@ -0,0 +1,17 @@
+//
+//  PfaukussApp.swift
+//  Pfaukuss
+//
+//  Created by Privat on 19.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PfaukussApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
